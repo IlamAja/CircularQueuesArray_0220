daa738ec7d39f5d0abb6f5a2386e88e33a9d4120
Ilam Ball <informasiilam@gmail.com>
2024-05-30 15:04:23 +0700

    membuat code untuk cek apakah antrian memiliki  1 elemen

--- linkedqueue.cpp
+++ linkedqueue.cpp
@@ -43,11 +43,17 @@ public:
 
     void remove() {
         // cek apakah antrian masih kosong
-        if (FRONT == -1) {
+        if (FRONT == -1){
             cout << "Queue underflow\n";
             return;
         }
         cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        // cek apakah antrian hanya memiliki satu elemen'
+        if (FRONT == REAR){
+            FRONT = -1;
+            REAR = -1;
+        }
         
     }
 };
